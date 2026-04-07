f1f3e0caec18be6274dac618202f4944e4c64b2c
tjsdn9138 <tjsdn9138@daum.net>
2026-04-07 20:12:05 +0900

    app development

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'todolist-v1'
+const FILES = [
+    './',
+    './index.html',
+]
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(FILES)
+        })
+    )
+})
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request)
+        })
+    )
+})
